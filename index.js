36d43fcdacebc1d64abfb604434eb939590fecc1
Nicolas Borboën <ponsfrilus@gmail.com>
2020-08-20 16:07:02 +0200

    [fix] little things

--- index.js
+++ index.js
@@ -6,39 +6,49 @@ if (!TOKEN) {
 }
 
 const me = 'ponsfrilus'
-const title = '### Hi there 👋'
+const title = '### Hi there 👋\n'
 
 const { Octokit } = require("@octokit/core")
 const octokit = new Octokit({ auth: TOKEN })
 
 const fs = require('fs')
 
+// adapted from https://stackoverflow.com/a/4312956/960623
+const monthDiff = (dateFrom, dateTo) => {
+  months = dateTo.getMonth() - dateFrom.getMonth() + 
+   (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
+  return Math.floor(months/12) + ' years and ' + months%12 + ' months';
+}
+
 const _generateREADMEheader = async () => {
   info = await octokit.request('GET /users/{username}', {
     username: me
   })
-  let header = `<p align="center">
+  let header = `\n<p align="center">
   <img src="${info.data.avatar_url}" />
   <h3 align="center">
-    ${info.data.name} aka ${info.data.login}
+    ${info.data.name} aka ‟${info.data.login}„
   </h3>
   <p align="center">
-    ${info.data.bio}
+    Hello world! I'm a full stack engineer working <a href="https://github.com/epfl-si">${info.data.company}</a> in ${info.data.location}.
+    Apparently, I'm hanging around on GitHub since ${monthDiff(new Date(info.data.created_at), new Date())}.
+    <br />You can reach me on twitter <a href="https://twitter.com/${info.data.twitter_username}">@${info.data.twitter_username}</a> or <a href="http://linkedin.com/in/nicolasborboen">LinkedIn</a>.
+    <br />I'm also a teacher, a bots breeder, a happy husband and father of 2.
   </p>
-</p>`
+  <p align="center">
+    <a href="https://www.epfl.ch">#EPFL</a> | 
+    <a href="https://github.com/epfl-si/">#EPFL-SI</a> | 
+    <a href="https://github.com/epfl-idevfsd">#EPFL-IDEVFSD</a> | 
+    <a href="https://github.com/topics/epfl-dojo">#EPFL-Dojo</a> | 
+    <a href="https://github.com/topics/epfl-dojo-kata">#EPFL-Dojo-kata</a> | 
+    <a href="https://en.wikipedia.org/wiki/Indentation_style#Variant:_1TBS_(OTBS)">#1TBS</a>
+  </p>
+  <p align="center">
+    <a href="https://github.com/ponsfrilus"><img alt="GitHub followers" src="https://img.shields.io/github/followers/ponsfrilus?label=Follow%20me%20on%20github&style=social"></a>
+    <a href="https://twitter.com/ponsfrilus"><img alt="Twitter Follow" src="https://img.shields.io/twitter/follow/ponsfrilus?label=follow%20me%20on%20twitter&style=social"></a>
+  </p>
+  </p><hr>`
   return header
-}
-
-const _generateStarredRepo = async () => {
-  repo = await octokit.request('GET /user/starred', {
-    per_page: 10
-  })
-  let ret = '<h3>My last 10 starred repo</h3>\n<ul>\n'
-  repo.data.forEach((item, i) => {
-    ret += `<li><a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a></li>`
-  })
-  ret += '</ul>'
-  return ret
 }
 
 const _generateLastCommits = async () => {
@@ -54,18 +64,40 @@ const _generateLastCommits = async () => {
     }
   })
 
-  let ret = '<h3>My last 10 commits</h3>\n<ul>\n'
+  let ret = '\n<table>\n<thead>\n<tr>\n<th>My last commits</th>\n</tr>\n</thead>\n<tr>\n<td valign="top">\n<ul>\n'
   commits.data.items.forEach((item, i) => {
-    ret += `<li><a href="${item.url}" title="${item.commit.author.date}" target="_blank">${item.sha.slice(0,8)}</a>@<a href="${item.repository.owner.url}">${item.repository.owner.login}</a>/<a href="${item.repository.html_url}" title="${item.repository.description}">${item.repository.name}</a><br/>${item.commit.message}</li>`
+    ret += `<li><a href="${item.url}" title="${item.commit.author.date}" target="_blank">${item.sha.slice(0,8)}</a><a href="${item.repository.owner.html_url}">@${item.repository.owner.login}</a><a href="${item.repository.html_url}" title="${item.repository.description}">/${item.repository.name}</a><br/><pre>${item.commit.message}</pre></li>\n`
   })
-  ret += '</ul>'
+  ret += '</ul>\n</td>\n</tr>\n</table>\n'
+  return ret
+}
+
+const _generateStarredRepo = async () => {
+  repo = await octokit.request('GET /user/starred', {
+    per_page: 20
+  })
+  let ret = '<table>\n<thead>\n<tr>\n<th colspan=2>My last starred repos</th>\n</tr>\n</thead>\n<tr>\n'
+  let retA = '\n<td valign="top">\n<ul>\n'
+  let retB = '\n<td valign="top">\n<ul>\n'
+  repo.data.forEach((item, i) => {
+    if (i%2) {
+      retB += `<li><a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a></li>\n`
+    } else {
+      retA += `<li><a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a></li>\n`
+    }
+  })
+  retA += '</ul>\n</td>\n'
+  retB += '</ul>\n</td>\n'
+  ret += retA + retB + '</td>\n</tr>\n</table>\n'
   return ret
 }
 
 const init = async () => {
   let content = title
   content += await _generateREADMEheader()
-  content += `<table><tr><td>${await _generateLastCommits()}</td><td style="vertical-align:top;width:40%">${await _generateStarredRepo()}</td></tr></table>`
+  content += `${await _generateLastCommits()}`
+  content += `${await _generateStarredRepo()}`
+  content += `<br><br><small>⏰ Updated on ${new Date().toUTCString()}</small>`
   fs.writeFile("./README.md", content, function(err) {
     if(err) {
       return console.log(err)
