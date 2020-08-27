724fbe9241753dbc87451c1c5f790530c2593a03
Nicolas Borboën <ponsfrilus@gmail.com>
2020-08-27 23:49:51 +0200

    [feature] layout improvements
    * now has a whole HTML table
    * the width should be fixed by an image (thanks to 
    https://stackoverflow.com/questions/50455704/full-width-markdown-tables)
    * new picture

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ const _generateREADMEheader = async () => {
     username: me
   })
   let header = `\n<p align="center">
-  <img src="${info.data.avatar_url}" />
+  <!-- use ${info.data.avatar_url} for your default github picture -->
+  <img src="https://raw.githubusercontent.com/ponsfrilus/ponsfrilus/master/img/ponsfrilus.png" title="${info.data.name} aka ‟${info.data.login}„" alt="${info.data.name} aka ‟${info.data.login}„" />
   <h3 align="center">
     ${info.data.name} aka ‟${info.data.login}„
   </h3>
   <p align="center">
     Hello world! I'm a full stack engineer working <a href="https://github.com/epfl-si">${info.data.company}</a> in ${info.data.location}.
-    Apparently, I'm hanging around on GitHub since ${monthDiff(new Date(info.data.created_at), new Date())}.
+    <br />Apparently, I'm hanging around on GitHub since ${monthDiff(new Date(info.data.created_at), new Date())}.
     <br />You can reach me on twitter <a href="https://twitter.com/${info.data.twitter_username}">@${info.data.twitter_username}</a> or <a href="http://linkedin.com/in/nicolasborboen">LinkedIn</a>.
     <br />I'm also a teacher, a bots breeder, a happy husband and father of 2.
   </p>
@@ -64,17 +65,21 @@ const _generateLastCommits = async () => {
     }
   })
 
-  let ret = '\n<table>\n<thead>\n<tr>\n<th>My last commits</th>\n</tr>\n</thead>\n<tr>\n<td valign="top">\n<ul>\n'
+  let ret = '<tr>\n<td colspan="2" align="center"><h4>My last commits</h4></td>\n</tr>\n'
   let num = 0
   commits.data.items.every((item, i) => {
     if (item.commit.message != 'README updated by GitHub Actions') {
       num++
-      ret += `<li><a href="${item.url}" title="${item.commit.author.date}" target="_blank">${item.sha.slice(0,8)}</a><a href="${item.repository.owner.html_url}">@${item.repository.owner.login}</a><a href="${item.repository.html_url}" title="${item.repository.description}">/${item.repository.name}</a><br/><pre>${item.commit.message}</pre></li>\n`
+      ret += `<tr>
+        <td colspan="2">
+          <div><strong><a href="${item.url}" title="${item.commit.author.date}" target="_blank">${item.sha.slice(0,8)}</a><a href="${item.repository.owner.html_url}">@${item.repository.owner.login}</a><a href="${item.repository.html_url}" title="${item.repository.description}">/${item.repository.name}</a></strong></div>
+          <pre>${item.commit.message}</pre>
+        </td>
+        </tr>`
     }
     if (num >= 10) return false
     else return true
   })
-  ret += '</ul>\n</td>\n</tr>\n</table>\n'
   return ret
 }
 
@@ -82,30 +87,32 @@ const _generateStarredRepo = async () => {
   repo = await octokit.request('GET /user/starred', {
     per_page: 20
   })
-  let ret = '<table>\n<thead>\n<tr>\n<th colspan=2>My last starred repos</th>\n</tr>\n</thead>\n<tr>\n'
-  let retA = '\n<td valign="top">\n<ul>\n'
-  let retB = '\n<td valign="top">\n<ul>\n'
+  let ret = '<tr>\n<td colspan="2" align="center"><h4>My last starred repos</h4></td>\n</tr>\n<tr>\n'
+  let retA = '<td valign="top">\n<ul>\n'
+  let retB = '<td valign="top">\n<ul>\n'
   repo.data.forEach((item, i) => {
     if (i%2) {
-      retB += `<li><a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a></li>\n`
+      retB += `<li>\n<a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a>\n</li>\n`
     } else {
-      retA += `<li><a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a></li>\n`
+      retA += `<li>\n<a href="${item.html_url}" title="${item.description}" target="_blank">${item.name}</a>&nbsp;<a href="${item.html_url}" title="${item.description}" target="_blank"><img src="https://img.shields.io/github/stars/${item.full_name}?style=social" alt="GitHub"></a>\n</li>\n`
     }
   })
-  retA += '</ul>\n</td>\n'
-  retB += '</ul>\n</td>\n'
-  ret += retA + retB + '</td>\n</tr>\n</table>\n'
+  retA += '</ul>\n<img width="450" height="1" /></td>\n'
+  retB += '</ul>\n<img width="450" height="1" /></td>\n'
+  ret += retA + retB + '</tr>\n'
   return ret
 }
 
 const init = async () => {
   let content = title
   content += await _generateREADMEheader()
-  content += `${await _generateLastCommits()}`
+  content += `<table align="center">\n`
   content += `${await _generateStarredRepo()}`
-  content += `<br><br><small>⏰ Updated on ${new Date().toUTCString()}</small>`
+  content += `${await _generateLastCommits()}`
+  content += `<tfoot>\n<tr>\n<td colspan="2" align="right">\n<img width="900" height="1" />\n<small>⏰ Updated on ${new Date().toUTCString()}</small>\n</td>\n</tr>\n</tfoot>\n<br />\n`
+  content += `</table>\n`
   fs.writeFile("./README.md", content, function(err) {
-    if(err) {
+    if (err) {
       return console.log(err)
     }
   })
